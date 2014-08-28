40b7d9b704730cdd595855eda13b898750029c6b
Oliver Erhart <oliver.erhart@gmail.com>
2014-08-28 23:33:26 +0200

    changed library structure and added aloha settings in external file

---
+++ public/js/aloha/aloha-config.js
@@ -0,0 +1,40 @@
+(function(window, undefined) {
+	var Aloha = window.Aloha || (window.Aloha = {});
+	Aloha.settings = {
+		_pluginBaseUrlByName : '.',
+		logLevels : {
+			'error' : true,
+			'warn' : true,
+			'info' : true,
+			'debug' : false,
+			'deprecated' : true
+		},
+		sidebar : {
+			disabled : true
+		},
+		errorhandling : false,
+		ribbon : false,
+		locale : 'de',
+		plugins : {
+			format : {
+				// all elements with no specific configuration get this
+				// configuration
+				config : [ 'b', 'i', 'p', 'sub', 'sup', 'del', 'title', 'h1',
+						'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'removeFormat' ],
+				editables : {
+					// no formatting allowed for title
+					'#top-text' : []
+				}
+			}
+		},
+		toolbar : {
+			tabs : [ {
+				label : 'Article.cls',
+				showOn : {
+					scope : 'Aloha.continuoustext'
+				},
+				components : [  'fontfamily', 'footnote' ] 
+			} ]
+		}
+	};
+})(window);
